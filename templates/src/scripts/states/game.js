aa143e0b261197780fe5e1d0bb6ea4db4c880598
Anthony Sapp <anthony@Venom-2.local>
2014-10-16 17:27:34 -0400

    changed some pathing, fixed gulp server

--- templates/src/scripts/states/game.js
+++ templates/src/scripts/states/game.js
@@ -1,16 +1,7 @@
-import RotateLogo from 'prefabs/rotate-logo';
-
-var logo;
 class Game {
 
     create() {
-        logo = new RotateLogo(
-            this.game,
-            this.game.width / 2, this.game.height / 2,
-            0.5
-        );
-
-        this.add.existing(logo);
+        console.log('Hello Game!');
     }
 
 }
